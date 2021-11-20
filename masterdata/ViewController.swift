06967f47af55ada1cd6b553ac6c831dd99ab5a44
Tomas Fernandez Velazco <tfernandez@bemobile.es>
2021-11-19 23:43:02 -0300

    Initial Commit

---
+++ masterdata/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  masterdata
+//
+//  Created by Tomas Fernandez Velazco on 19/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
